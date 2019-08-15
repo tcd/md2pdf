24377ff905152e034bcce8fa798f79794f1a2301
Clay Dunston <claydunston@gmail.com>
2019-08-14 21:06:27 -0500

    Clean HTML escapes before parsing

--- md2pdf/clean.go
+++ md2pdf/clean.go
@@ -1,9 +1,40 @@
 package md2pdf
 
+import (
+	"strings"
+)
+
+// https://www.w3.org/wiki/Common_HTML_entities_used_for_typography
 var escapes = map[string]string{
-	"&quot;":  `"`,
-	"&amp;":   "&",
-	"&lt;":    "<",
-	"&gt;":    ">",
-	"&rsquo;": "'",
+	"&quot;": `"`,
+	"&amp;":  "&",
+	"&lt;":   "<",
+	"&gt;":   ">",
+	// "&rdquo;":  `"`,
+	// "&rsquo;":  "'",
+	// "&mdash;":  "",
+	// "&ndash;":  "",
+	// "&copy;":   "",
+	// "&trade;":  "",
+	// "&reg;":    "",
+	// "&hellip;": "",
+	// "&frac12;": "",
+	// "&frac14;": "",
+	// "&frac34;": "",
+}
+
+func cleanHTML(html string) string {
+	s1 := strings.ReplaceAll(html, "&quot;", `"`)
+	s2 := strings.ReplaceAll(s1, "&amp;", "&")
+	s3 := strings.ReplaceAll(s2, "&lt;", "<")
+	s4 := strings.ReplaceAll(s3, "&gt;", ">")
+	return s4
+
+	// r := strings.NewReplacer( // TODO: File an issue about strings.NewReplacer. It missed some '&lt;'s and '&gt;'s.
+	// 	"&quot;", `"`,
+	// 	"&amp;", "&",
+	// 	"&lt;", "<",
+	// 	"&gt;", ">",
+	// )
+	// return r.Replace(html)
 }
