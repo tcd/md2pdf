c892ad5f1e8020b2519feada568d8e4549bcd64b
Clay Dunston <claydunston@gmail.com>
2019-08-15 18:30:34 -0500

    Clean up parse functions file

--- md2pdf/parse_funcs.go
+++ md2pdf/parse_funcs.go
@@ -8,32 +8,6 @@ import (
 
 // func parse(pdf *gofpdf.Fpdf, z *html.Tokenizer) {}
 
-// func parseA(pdf *gofpdf.Fpdf, z *html.Tokenizer, token html.Token) {
-// 	var content, href string
-//
-// 	for _, a := range token.Attr {
-// 		if a.Key == "href" {
-// 			href = a.Val
-// 		}
-// 	}
-//
-// 	tt := z.Next()
-// 	if tt == html.StartTagToken {
-// 		fmt.Println("Aint nobody got time for that")
-// 	}
-// 	if tt == html.TextToken {
-// 		T1 := z.Text()
-// 		tt2 := z.Next()
-// 		if tt2 == html.EndTagToken {
-// 			content = string(T1)
-// 		} else {
-// 			fmt.Println("Aint nobody got time for that")
-// 		}
-// 	}
-//
-// 	// link(pdf, content, href)
-// }
-
 func parseEm(pdf *gofpdf.Fpdf, z *html.Tokenizer) {}
 
 func parseStrong(pdf *gofpdf.Fpdf, z *html.Tokenizer) {}
@@ -45,34 +19,17 @@ func parsePre(pdf *gofpdf.Fpdf, z *html.Tokenizer) {
 	if tt == html.StartTagToken {
 		T1 := z.Token()
 		if T1.Data == "code" {
-			// for _, a := range T1.Attr {
-			// 	if a.Key == "class" {
-			// 		class := a.Val // Use this for syntax highlighting.
-			// 	}
-			// }
 			tt2 := z.Next()
 			if tt2 == html.TextToken {
 				content := z.Text()
 				CodeBlock(pdf, string(content))
 			}
-			// tt3 := z.Next()
-			// if tt3 == html.EndTagToken {
-			// 	if T1.Data == "code" {
-			// 		tt4 := z.Next()
-			// 		if tt4 == html.EndTagToken {
-			// 			if T1.Data == "pre" {
-			// 				CodeBlock(pdf, string(content))
-			// 			}
-			// 		}
-			// 	}
-			// }
 		}
 	}
-
-	// if tt == html.TextToken {
-	// 	content := z.Text()
-	// 	CodeBlock(pdf, string(content))
-	// }
+	if tt == html.TextToken {
+		content := z.Text()
+		CodeBlock(pdf, string(content))
+	}
 }
 
 func parseH1(pdf *gofpdf.Fpdf, z *html.Tokenizer) {
