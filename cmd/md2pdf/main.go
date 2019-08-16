eb252054940db527fbc09aac9449a35d5831ca41
Clay Dunston <claydunston@gmail.com>
2019-08-16 13:01:31 -0500

    Update cmd

--- cmd/md2pdf/main.go
+++ cmd/md2pdf/main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/tcd/md2pdf/internal/ghfm"
+	"github.com/tcd/md2pdf/md2pdf"
+)
+
 func main() {
 	runParse()
-	// cleanHTML()
+}
+
+const (
+	pdfOutDir    = "../../out/pdf/"
+	htmlOutDir   = "../../out/html/"
+	readme       = "../../readme.md"
+	mdCheatsheet = "../../static/markdown-cheatsheet.md"
+)
+
+func runParse() {
+	ghfm.GitHubPDF(pdfOutDir + "table3.pdf")
+	// parse.GenAST()
+
+	err := md2pdf.Md2PDF(mdCheatsheet, pdfOutDir+"github3.pdf")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
