caca74d2d73c74b9886345fc39e69c0edad171b4
Clay Dunston <claydunston@gmail.com>
2019-08-15 21:02:25 -0500

    Don't render empty paragraphs

--- internal/ghfm/ghfm.go
+++ internal/ghfm/ghfm.go
@@ -121,6 +121,9 @@ func CodeBlock(f *gofpdf.Fpdf, text string) {
 // BasicP writes the contents of a paragraph without rendering
 // any enclosed elements.
 func BasicP(f *gofpdf.Fpdf, text string) {
+	if len(text) == 0 {
+		return
+	}
 	f.SetFont("helvetica", "", 12)
 	f.SetFillColor(255, 255, 255)
 	f.SetTextColor(36, 41, 46)
