59ed743836b0e0ca85278c6078a2479790323d90
Clay Dunston <claydunston@gmail.com>
2019-08-16 16:51:53 -0500

    Add basic blockquote rendering function

--- internal/ghfm/ghfm.go
+++ internal/ghfm/ghfm.go
@@ -129,3 +129,21 @@ func BasicP(f *gofpdf.Fpdf, text string) {
 	f.Write(6, text)
 	f.Ln(11)
 }
+
+// BasicBlockquote writes text to a blockquote without rendering
+// any enclosed elements.
+func BasicBlockquote(f *gofpdf.Fpdf, text string) {
+	oldCellMargin := f.GetCellMargin()
+
+	f.SetFont("helvetica", "", 12)
+	f.SetTextColor(106, 115, 125)
+	f.SetDrawColor(223, 226, 229)
+	f.SetLineWidth(1)
+	f.SetCellMargin(4)
+
+	_, lineHeight := f.GetFontSize()
+	f.MultiCell(0, lineHeight*1.5, text, "L", "", false)
+	f.Ln(6)
+
+	f.SetCellMargin(oldCellMargin)
+}
