1c05c5381bc25097c1fbd7b5410dc3e3c5a278ad
Clay Dunston <claydunston@gmail.com>
2019-08-16 19:01:47 -0500

    Move content functions to an internal directory

--- internal/content/content.go
+++ internal/content/content.go
@@ -1,14 +1,19 @@
-package ghfm
+// Package content returns strings containing markdown content.
+package content
 
-import "strings"
+import (
+	"strings"
 
-// lorem returns 10 sentences of Lorem Ipsum text.
-func lorem() string {
+	"github.com/tcd/md2pdf/internal/render"
+)
+
+// Lorem returns 10 sentences of Lorem Ipsum text.
+func Lorem() string {
 	return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel dapibus orci. Donec nec dignissim lectus. Cras nisi diam, hendrerit quis ex ut, porttitor posuere augue. Nulla elit ligula, laoreet quis egestas quis, interdum eu sem. Etiam luctus, diam in lacinia facilisis, dui sem iaculis lacus, eget porttitor est quam non sapien. Fusce vestibulum accumsan interdum. Vivamus tristique congue tincidunt. Duis molestie turpis vel varius maximus. Fusce tristique dolor arcu, id feugiat tellus condimentum ac."
 }
 
-// lorem2 returns a long paragraph of Lorem Ipsum text.
-func lorem2() string {
+// Lorem2 returns a long paragraph of Lorem Ipsum text.
+func Lorem2() string {
 	return `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel dapibus orci. Donec nec dignissim lectus. Cras nisi diam, hendrerit quis ex ut, porttitor posuere augue. Nulla elit ligula, laoreet quis egestas quis, interdum eu sem. Etiam luctus, diam in lacinia facilisis, dui sem iaculis lacus, eget porttitor est quam non sapien. Fusce vestibulum accumsan interdum. Vivamus tristique congue tincidunt. Duis molestie turpis vel varius maximus. Fusce tristique dolor arcu, id feugiat tellus condimentum ac.
 In hac habitasse platea dictumst. Proin libero lectus, rutrum in porttitor in, mattis vitae nulla. Nulla aliquet purus nec magna ultricies tincidunt. Etiam dignissim ultrices leo luctus feugiat. Nunc congue vitae urna sed gravida. Praesent viverra leo nulla, sit amet sodales magna ultricies a. Aenean ornare semper enim, sed ultrices est gravida eget. Nullam pharetra lorem id ullamcorper maximus. Ut nisi felis, vehicula rutrum vehicula non, volutpat eu ante. Donec aliquam lectus sit amet ultrices aliquet. Nullam tempus interdum ipsum, vitae tincidunt mi dictum eleifend. Aenean vitae ante id justo convallis volutpat id sed dolor. Nam quis lectus eget nunc porttitor commodo vitae et sapien.
 Cras vitae sagittis purus. Mauris rutrum posuere mattis. Aenean enim tortor, suscipit sed aliquam et, ornare ac neque. Suspendisse feugiat augue laoreet aliquam viverra. Sed eleifend lacus sit amet dictum volutpat. Phasellus sit amet libero tincidunt, eleifend nulla vitae, consequat orci. Curabitur ante mauris, finibus at eleifend at, porta a dolor. Praesent varius eros id quam semper pretium. Nunc non magna ut urna aliquam cursus. Etiam ut bibendum tortor, eget iaculis sem. Fusce sodales porta arcu, ac convallis lectus porta non. Morbi non interdum ex. Sed pellentesque imperdiet sodales.
@@ -16,7 +21,8 @@ Nullam vestibulum sagittis ultrices. In dignissim non justo quis iaculis. Etiam 
 Curabitur quis convallis risus, in tristique felis. In facilisis arcu sagittis egestas semper. Sed eu ex at dolor pellentesque eleifend. Nam metus leo, dictum quis ultricies sed, vulputate et erat. Integer vel fringilla nunc. Ut vulputate pellentesque ante, vel sodales nunc consequat ut. Aenean dictum tristique sem, a gravida erat pretium ut. Sed sagittis pulvinar ipsum nec fermentum. Donec gravida, metus ut ultrices consequat, est felis pharetra nulla, eget sodales arcu sem pellentesque dui.`
 }
 
-func loremList() []string {
+// LoremList returns a slice of placeholder strings.
+func LoremList() []string {
 	return []string{
 		"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
 			"tempor incididunt ut labore et dolore magna aliqua.",
@@ -29,7 +35,8 @@ func loremList() []string {
 	}
 }
 
-func cbContent() string {
+// HeaderContent returns a string with different level headers.
+func HeaderContent() string {
 	return `# H1
 ## H2
 ### H3
@@ -46,7 +53,8 @@ Alt-H2
 ------`
 }
 
-func emContent() string {
+// EmContent returns a string with bold, italic, and stikeout elements.
+func EmContent() string {
 	return `Emphasis, aka italics, with *asterisks* or _underscores_.
 
 Strong emphasis, aka bold, with **asterisks** or __underscores__.
@@ -56,7 +64,8 @@ Combined emphasis with **asterisks and _underscores_**.
 Strikethrough uses two tildes. ~~Scratch this.~~`
 }
 
-func dirtyListContent() string {
+// DirtyListContent returns a markdown list with unicode characters that can't be rendered properly by gofpdf.
+func DirtyListContent() string {
 	return `1. First ordered list item
 2. Another item
 ⋅⋅* Unordered sub-list.
@@ -75,7 +84,8 @@ func dirtyListContent() string {
 + Or pluses`
 }
 
-func listContent() string {
+// ListContent returns a string containing several markdown lists.
+func ListContent() string {
 	return `1. First ordered list item
 2. Another item
   * Unordered sub-list.
@@ -94,7 +104,8 @@ func listContent() string {
 + Or pluses`
 }
 
-func linksContent() string {
+// LinksContent returns a string containing several markdown links
+func LinksContent() string {
 	return `[I'm an inline-style link](https://www.google.com)
 
 [I'm an inline-style link with title](https://www.google.com "Google's Homepage")
@@ -118,7 +129,8 @@ Some text to show that the reference links can follow later.
 [link text itself]: http://www.reddit.com`
 }
 
-func imagesContent() string {
+// ImagesContent returns a string containing several markdown images.
+func ImagesContent() string {
 	return `Here's our logo (hover to see the title text):
 
 Inline-style:
@@ -130,11 +142,13 @@ Reference-style:
 [logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"`
 }
 
-func inlineCodeContent() string {
+// InlineCodeContent returns a string with some words wrapped in backticks.
+func InlineCodeContent() string {
 	return "Inline `code` has `back-ticks around` it."
 }
 
-func syntaxHighContent() string {
+// SyntaxHighContent returns a string containing markdown codeblocks.
+func SyntaxHighContent() string {
 	content := []string{
 		"```javascript",
 		`var s = "JavaScript syntax highlighting";`,
@@ -155,7 +169,8 @@ func syntaxHighContent() string {
 	return strings.Join(content, "\n")
 }
 
-func tablesContent() string {
+// TablesContent returns a string containing markdown tables.
+func TablesContent() string {
 	return `Colons can be used to align columns.
 
 | Tables        | Are           | Cool  |
@@ -175,7 +190,8 @@ Markdown | Less | Pretty
 `
 }
 
-func blockquoteContent() string {
+// BlockquoteContent returns markdown blockquotes.
+func BlockquoteContent() string {
 	return `> Blockquotes are very handy in email to emulate reply text.
 > This line is part of the same quote.
 
@@ -184,7 +200,8 @@ Quote break.
 > This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote. `
 }
 
-func inlineHTMLContent() string {
+// InlineHTMLContent returns an html definition list.
+func InlineHTMLContent() string {
 	return `<dl>
   <dt>Definition list</dt>
     <dd>Is something people use sometimes.</dd>
@@ -195,7 +212,8 @@ func inlineHTMLContent() string {
 `
 }
 
-func hrContent() string {
+// HRContent returns a string with several horizontal rules.
+func HRContent() string {
 	return `Three or more...
 
 ---
@@ -211,7 +229,8 @@ ___
 Underscores`
 }
 
-func lineBreakContents() string {
+// LineBreakContents returns strings interspersed with empty lines.
+func LineBreakContents() string {
 	return `Here's a line for us to start with.
 
 This line is separated from the one above by two newlines, so it will be a *separate paragraph*.
@@ -220,18 +239,21 @@ This line is also a separate paragraph, but...
 This line is only separated by a single newline, so it's a separate line in the *same paragraph*.`
 }
 
-func youTubeContent1() string {
+// YouTubeContent1 returns an HTML link to a YouTube video.
+func YouTubeContent1() string {
 	return `<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_VIDEO_ID_HERE" target="_blank">
   <img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg" alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" />
 </a>`
 }
 
-func youTubeContent2() string {
+// YouTubeContent2 returns a markdown link to a YouTube video.
+func YouTubeContent2() string {
 	return "[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](http://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID_HERE)"
 }
 
-func tableContent1() TableContent {
-	return TableContent{
+// TableContent1 returns TableContent data.
+func TableContent1() render.TableContent {
+	return render.TableContent{
 		Rows: [][]string{
 			{"Tables", "Are", "Cool"},
 			{"col 3 is", "right-aligned", "$1600"},
@@ -242,8 +264,9 @@ func tableContent1() TableContent {
 	}
 }
 
-func tableContent2() TableContent {
-	return TableContent{
+// TableContent2 returns TableContent data.
+func TableContent2() render.TableContent {
+	return render.TableContent{
 		Rows: [][]string{
 			{"Markdown", "Less", "Pretty"},
 			{"still", "renders", "nicely"},
@@ -253,7 +276,8 @@ func tableContent2() TableContent {
 	}
 }
 
-var tocContent = []string{
+// TocContent returns a slice of short strings.
+var TocContent = []string{
 	"Headers",
 	"Emphasis",
 	"Lists",
@@ -266,4 +290,32 @@ var tocContent = []string{
 	"Horizontal Rule",
 	"Line Breaks",
 	"YouTube Videos",
+}
+
+// ListContent1 returns a slice of string.
+var ListContent1 = []string{
+	"First ordered list item",
+	"Another item",
+	"Unordered sub-list.",
+}
+
+// ListContent2 returns a slice of string.
+var ListContent2 = []string{
+	"Actual numbers don't matter, just that it's a number",
+	"Ordered sub-list",
+	`And another item.
+
+You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
+
+To have a line break without a paragraph, you will need to use two trailing spaces.
+Note that this line is separate, but within the same paragraph.
+(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
+`,
+}
+
+// ListContent3 returns a slice of string.
+var ListContent3 = []string{
+	"Unordered list can use asterisks",
+	"Or minuses",
+	"Or pluses",
 }
