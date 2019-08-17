1da753bb3e5312353b9b031d18af74be24d16674
Clay Dunston <claydunston@gmail.com>
2019-08-16 19:29:57 -0500

    Update import path

--- cmd/md2pdf/main.go
+++ cmd/md2pdf/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/tcd/md2pdf/internal/md2pdf"
 )
 
@@ -12,15 +14,15 @@ const (
 	pdfOutDir    = "../../out/pdf/"
 	htmlOutDir   = "../../out/html/"
 	readme       = "../../readme.md"
-	mdCheatsheet = "../../static/markdown-cheatsheet.md"
+	mdCheatsheet = "../../testdata/markdown/markdown-cheatsheet.md"
 )
 
 func runParse() {
-	md2pdf.GitHubPDF(pdfOutDir + "list2.pdf")
+	// md2pdf.GitHubPDF(pdfOutDir + ".pdf")
 	// parse.GenAST()
 
-	// err := md2pdf.Md2PDF(mdCheatsheet, pdfOutDir+"newest.pdf")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	err := md2pdf.Md2PDF(mdCheatsheet, pdfOutDir+"newest.pdf")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
