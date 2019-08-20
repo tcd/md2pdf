25c13ec1270a67f8f5115d4f53cf223a6197c663
Clay Dunston <claydunston@gmail.com>
2019-08-20 15:24:45 -0500

    Move Text model to internal/render/render.go

--- internal/render/render.go
+++ internal/render/render.go
@@ -1,7 +1,11 @@
 // Package render renders text in the style of GitHub using gofpdf.
 package render
 
-import "github.com/jung-kurt/gofpdf"
+import (
+	"time"
+
+	"github.com/jung-kurt/gofpdf"
+)
 
 const (
 	pageWidth = 176 // (216 - 40 for margins)
@@ -14,4 +18,52 @@ func Setup(f *gofpdf.Fpdf) {
 	f.SetMargins(20, 13, 20)
 	f.SetFillColor(255, 255, 255)
 	f.SetTextColor(36, 41, 46)
+}
+
+func setMetaData(f *gofpdf.Fpdf, author, title string) {
+	f.SetTitle(title, true)
+	f.SetAuthor(author, true)
+	f.SetCreator(author, true)
+	f.SetCreationDate(time.Now())
+}
+
+// Text models text strings that make up a Paragraph, Blockquote, List Item, or Table Cell.
+type Text struct {
+	Content string
+	Bold    bool
+	Italic  bool
+	Code    bool
+	Strike  bool
+	HREF    string
+}
+
+// Contents models the contents of a Paragraph, Blockquote, List Item, or Table Cell.
+type Contents struct {
+	Content []Text
+}
+
+// AddContent to a to Contents.
+func (p *Contents) AddContent(text Text) {
+	p.Content = append(p.Content, text)
+}
+
+// Copy returns a pointer to a new Text struct with the same
+// values as the Text it was called from.
+// Except the Content field, that's empty.
+func (txt Text) Copy() *Text {
+	var newText Text
+	if txt.Bold {
+		newText.Bold = true
+	}
+	if txt.Italic {
+		newText.Italic = true
+	}
+	if txt.Code {
+		newText.Code = true
+	}
+	if txt.Strike {
+		newText.Strike = true
+	}
+	newText.HREF = txt.HREF
+	return &newText
 }
