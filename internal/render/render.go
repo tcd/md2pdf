d684a9e4a97068503b725210afe4cb24b05029d9
Clay Dunston <claydunston@gmail.com>
2019-08-21 16:51:09 -0500

    Move Text struct to its own file

--- internal/render/render.go
+++ internal/render/render.go
@@ -20,50 +20,10 @@ func Setup(f *gofpdf.Fpdf) {
 	f.SetTextColor(36, 41, 46)
 }
 
-func setMetaData(f *gofpdf.Fpdf, author, title string) {
+// SetMetaData for a pdf.
+func SetMetaData(f *gofpdf.Fpdf, author, title string) {
 	f.SetTitle(title, true)
 	f.SetAuthor(author, true)
 	f.SetCreator(author, true)
 	f.SetCreationDate(time.Now())
-}
-
-// Text models text strings that make up a Paragraph, Blockquote, List Item, or Table Cell.
-type Text struct {
-	Content string
-	Bold    bool
-	Italic  bool
-	Code    bool
-	Strike  bool
-	HREF    string
-}
-
-// Contents models the contents of a Paragraph, Blockquote, List Item, or Table Cell.
-type Contents struct {
-	Content []Text
-}
-
-// AddContent to a to Contents.
-func (p *Contents) AddContent(text Text) {
-	p.Content = append(p.Content, text)
-}
-
-// Copy returns a pointer to a new Text struct with the same
-// values as the Text it was called from.
-// Except the Content field, that's empty.
-func (txt Text) Copy() *Text {
-	var newText Text
-	if txt.Bold {
-		newText.Bold = true
-	}
-	if txt.Italic {
-		newText.Italic = true
-	}
-	if txt.Code {
-		newText.Code = true
-	}
-	if txt.Strike {
-		newText.Strike = true
-	}
-	newText.HREF = txt.HREF
-	return &newText
 }
---
+++ internal/render/text.go
@@ -0,0 +1,50 @@
+package render
+
+// Text models text strings that make up a Paragraph, Blockquote, List Item, or Table Cell.
+type Text struct {
+	Content string `json:"content"`
+	Bold    bool   `json:"bold"`
+	Italic  bool   `json:"italic"`
+	Code    bool   `json:"code"`
+	Strike  bool   `json:"strike"`
+	HREF    string `json:"href"`
+}
+
+// Contents models the contents of a Paragraph, Blockquote, List Item, or Table Cell.
+type Contents struct {
+	Content []Text `json:"content"`
+}
+
+// AddContent to a to Contents.
+func (c *Contents) AddContent(text Text) {
+	c.Content = append(c.Content, text)
+}
+
+// AddStr adds a string with no styles to Contents.
+func (c *Contents) AddStr(str string) {
+	text := Text{
+		Content: str,
+	}
+	c.Content = append(c.Content, text)
+}
+
+// Copy returns a pointer to a new Text struct with the same
+// values as the Text it was called from.
+// Except the Content field, that's empty.
+func (txt Text) Copy() Text {
+	var newText Text
+	if txt.Bold {
+		newText.Bold = true
+	}
+	if txt.Italic {
+		newText.Italic = true
+	}
+	if txt.Code {
+		newText.Code = true
+	}
+	if txt.Strike {
+		newText.Strike = true
+	}
+	newText.HREF = txt.HREF[:]
+	return newText
+}
