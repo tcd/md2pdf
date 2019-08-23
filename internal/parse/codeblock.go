4dd21fbd997236a4f78c222ffbf2e6bf560f0115
Clay Dunston <claydunston@gmail.com>
2019-08-22 22:04:19 -0500

    Update parseCodeBlock to get a class name

--- internal/parse/codeblock.go
+++ internal/parse/codeblock.go
@@ -8,18 +8,33 @@ import (
 
 func parseCodeblock(pdf *gofpdf.Fpdf, z *html.Tokenizer) {
 	tt := z.Next()
+	this := render.Contents{}
+	var class string
+
+	if tt == html.TextToken {
+		content := z.Text()
+		this.AddStr(string(content))
+	}
+
 	if tt == html.StartTagToken {
 		T1 := z.Token()
 		if T1.Data == "code" {
+			for _, a := range T1.Attr {
+				if a.Key == "class" {
+					class = a.Val
+				}
+			}
 			tt2 := z.Next()
 			if tt2 == html.TextToken {
 				content := z.Text()
-				render.CodeBlock(pdf, string(content))
+				this.AddStr(string(content))
 			}
 		}
 	}
-	if tt == html.TextToken {
-		content := z.Text()
-		render.CodeBlock(pdf, string(content))
+
+	if class == "language-no-highlight" || class == "" {
+		render.CodeBlock(pdf, this)
+	} else {
+		render.CodeBlock(pdf, this)
 	}
 }
