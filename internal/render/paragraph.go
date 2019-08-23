2f678d3e9de4f2bc000756b3a85555838f547464
Clay Dunston <claydunston@gmail.com>
2019-08-22 21:07:19 -0500

    Update paragraph render functions to take Text

--- internal/render/paragraph.go
+++ internal/render/paragraph.go
@@ -46,9 +46,9 @@ func drawParagraphContent(f *gofpdf.Fpdf, c Contents) {
 		styleStr := styles.String()
 
 		if txt.Strike {
-			pStrike(f, txt.Content, txt.HREF)
+			pStrike(f, txt)
 		} else if txt.Code {
-			pInlineCode(f, txt.Content, txt.HREF)
+			pInlineCode(f, txt)
 		} else if txt.HREF != "" {
 			f.SetFont("helvetica", styleStr, 12)
 			f.SetTextColor(3, 102, 214)
@@ -68,36 +68,36 @@ func pSpan(f *gofpdf.Fpdf, text string, styleStr string) {
 	f.Write(lineHt, text)
 }
 
-func pInlineCode(f *gofpdf.Fpdf, text, href string) {
+func pInlineCode(f *gofpdf.Fpdf, txt Text) {
 	oldCellMargin := f.GetCellMargin()
 	f.SetFillColor(243, 243, 243)
 	f.SetFont("courier", "", 12)
-	width := (f.GetStringWidth(text) * 1.05)
+	width := (f.GetStringWidth(txt.Content) * 1.05)
 
-	if len(href) != 0 {
+	if txt.HREF != "" {
 		f.SetTextColor(3, 102, 214)
 		f.CellFormat(
-			width, // width; If 0, the cell extends up to the right margin.
-			6,     // height;
-			text,  // txtStr;
-			"",    // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
-			0,     // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
-			"LM",  // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
-			true,  // fill; true for color, false for transparent
-			0,     // link;  Identifier returned by AddLink() or 0 for no internal link.
-			href,  // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
+			width,       // width; If 0, the cell extends up to the right margin.
+			6,           // height;
+			txt.Content, // txtStr;
+			"",          // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
+			0,           // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
+			"LM",        // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
+			true,        // fill; true for color, false for transparent
+			0,           // link;  Identifier returned by AddLink() or 0 for no internal link.
+			txt.HREF,    // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
 		)
 	} else {
 		f.CellFormat(
-			width, // width; If 0, the cell extends up to the right margin.
-			6,     // height;
-			text,  // txtStr;
-			"",    // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
-			0,     // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
-			"LM",  // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
-			true,  // fill; true for color, false for transparent
-			0,     // link;  Identifier returned by AddLink() or 0 for no internal link.
-			"",    // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
+			width,       // width; If 0, the cell extends up to the right margin.
+			6,           // height;
+			txt.Content, // txtStr;
+			"",          // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
+			0,           // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
+			"LM",        // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
+			true,        // fill; true for color, false for transparent
+			0,           // link;  Identifier returned by AddLink() or 0 for no internal link.
+			"",          // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
 		)
 	}
 
@@ -105,11 +105,11 @@ func pInlineCode(f *gofpdf.Fpdf, text, href string) {
 }
 
 // Strike writes a line of text with a line through it.
-func pStrike(f *gofpdf.Fpdf, text string, href string) {
+func pStrike(f *gofpdf.Fpdf, txt Text) {
 	f.SetFont("helvetica", "", 12)
 	f.SetFillColor(255, 255, 255)
 	f.SetTextColor(36, 41, 46)
-	width := f.GetStringWidth(text)
+	width := f.GetStringWidth(txt.Content)
 	var lineHt float64 = 6
 
 	// Where the text starts, also where to start the strikethrough line.
@@ -119,11 +119,11 @@ func pStrike(f *gofpdf.Fpdf, text string, href string) {
 	f.SetLineWidth(0.25)
 	f.SetDrawColor(36, 41, 46)
 
-	if len(href) != 0 {
+	if txt.HREF != "" {
 		f.SetTextColor(3, 102, 214)
-		f.WriteLinkString(lineHt, text, href)
+		f.WriteLinkString(lineHt, txt.Content, txt.HREF)
 	} else {
-		f.Write(lineHt, text)
+		f.Write(lineHt, txt.Content)
 	}
 
 	f.Line(x, y, x+width, y)
