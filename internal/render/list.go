2bb4484abf501dfa1bf9510280084aa5544bbb23
Clay Dunston <claydunston@gmail.com>
2019-08-23 16:01:57 -0500

    Update list rendering

--- internal/render/list.go
+++ internal/render/list.go
@@ -38,83 +38,86 @@ const (
 )
 
 // AnyList writes an unordered list with up to three levels of indentation to a gofpdf.Fpdf.
-func AnyList(f *gofpdf.Fpdf, list List) {
-	f.SetLeftMargin(levelOneMargin)
-	f.SetRightMargin(21)
-	f.SetFont("helvetica", "", 12)
-	_, lineHt := f.GetFontSize() // need to call this after we call SetFont.
+func AnyList(pdf *gofpdf.Fpdf, list List) {
+	pdf.SetLeftMargin(levelOneMargin)
+	pdf.SetRightMargin(21)
+	pdf.SetFont("helvetica", "", 12)
+	_, lineHt := pdf.GetFontSize() // need to call this after we call SetFont.
 	lineHt = lineHt * 1.5
 
 	for _, item := range list.Items {
-		levelOneBullett(f, lineHt)
-		drawListItemContent(f, item.Contents, lineHt)
-		f.SetLeftMargin(levelOneMargin)
-		f.Ln(-1) // A negative value indicates the height of the last printed cell.
-		f.Ln(2)
+		levelOneBullett(pdf, lineHt)
+		drawListItemContent(pdf, item.Contents, lineHt)
+		pdf.SetLeftMargin(levelOneMargin)
+		pdf.Ln(-1) // A negative value indicates the height of the last printed cell.
+		pdf.Ln(2)
 
 		if item.HasChildren() {
-			f.SetLeftMargin(levelTwoMargin)
+			pdf.SetLeftMargin(levelTwoMargin)
 			lastIndex := len(item.Children.Items) - 1
 			for i, item := range item.Children.Items {
-				levelTwoBullet(f, lineHt)
-				drawListItemContent(f, item.Contents, lineHt)
-				f.SetLeftMargin(levelTwoMargin)
+				levelTwoBullet(pdf, lineHt)
+				drawListItemContent(pdf, item.Contents, lineHt)
+				pdf.SetLeftMargin(levelTwoMargin)
 				if i != lastIndex {
-					f.Ln(-1)
-					f.Ln(2)
+					pdf.Ln(-1)
+					pdf.Ln(2)
 				}
 				if item.HasChildren() {
-					f.Ln(-1)
-					f.Ln(2)
-					f.SetLeftMargin(levelThreeMargin)
+					pdf.Ln(-1)
+					pdf.Ln(2)
+					pdf.SetLeftMargin(levelThreeMargin)
 					lastIndex := len(item.Children.Items) - 1
 					for i, item := range item.Children.Items {
-						levelThreeBullet(f, lineHt)
-						drawListItemContent(f, item.Contents, lineHt)
-						f.SetLeftMargin(levelThreeMargin)
+						levelThreeBullet(pdf, lineHt)
+						drawListItemContent(pdf, item.Contents, lineHt)
+						pdf.SetLeftMargin(levelThreeMargin)
 						if i != lastIndex {
-							f.Ln(-1)
-							f.Ln(2)
+							pdf.Ln(-1)
+							pdf.Ln(2)
 						}
 					}
-					f.SetLeftMargin(levelTwoMargin)
-					f.Ln(-1)
-					f.Ln(2)
+					pdf.SetLeftMargin(levelTwoMargin)
+					pdf.Ln(-1)
+					pdf.Ln(2)
 				}
 			}
-			f.SetLeftMargin(levelOneMargin)
-			f.Ln(-1)
-			f.Ln(2)
+			pdf.SetLeftMargin(levelOneMargin)
+			pdf.Ln(-1)
+			pdf.Ln(2)
 		}
 	}
 
-	f.SetLeftMargin(20)
-	f.SetRightMargin(20)
-	f.Ln(4)
+	pdf.SetLeftMargin(20)
+	pdf.SetRightMargin(20)
+	pdf.Ln(4)
 }
 
-func levelOneBullett(f *gofpdf.Fpdf, lineHt float64) {
-	f.SetFont("zapfdingbats", "", 5)
+func levelOneBullett(pdf *gofpdf.Fpdf, lineHt float64) {
+	pdf.SetTextColor(36, 41, 46)
+	pdf.SetFont("zapfdingbats", "", 5)
 	bulletChar := "\x6c" // ●
-	f.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
-	f.SetLeftMargin(f.GetX())
+	pdf.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
+	pdf.SetLeftMargin(pdf.GetX())
 }
 
-func levelTwoBullet(f *gofpdf.Fpdf, lineHt float64) {
-	f.SetFont("zapfdingbats", "", 5)
+func levelTwoBullet(pdf *gofpdf.Fpdf, lineHt float64) {
+	pdf.SetTextColor(36, 41, 46)
+	pdf.SetFont("zapfdingbats", "", 5)
 	bulletChar := "\x6d" // ❍
-	f.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
-	f.SetLeftMargin(f.GetX())
+	pdf.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
+	pdf.SetLeftMargin(pdf.GetX())
 }
 
-func levelThreeBullet(f *gofpdf.Fpdf, lineHt float64) {
-	f.SetFont("zapfdingbats", "", 5)
+func levelThreeBullet(pdf *gofpdf.Fpdf, lineHt float64) {
+	pdf.SetTextColor(36, 41, 46)
+	pdf.SetFont("zapfdingbats", "", 5)
 	bulletChar := "\x6e" // ■
-	f.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
-	f.SetLeftMargin(f.GetX())
+	pdf.CellFormat(5, lineHt, bulletChar, "", 0, "RM", false, 0, "")
+	pdf.SetLeftMargin(pdf.GetX())
 }
 
-func drawListItemContent(f *gofpdf.Fpdf, c Contents, lineHt float64) {
+func drawListItemContent(pdf *gofpdf.Fpdf, c Contents, lineHt float64) {
 	for _, txt := range c.Content {
 		var styles strings.Builder // "B" (bold), "I" (italic), "U" (underscore) or any combination.
 		if txt.Bold {
@@ -126,82 +129,93 @@ func drawListItemContent(f *gofpdf.Fpdf, c Contents, lineHt float64) {
 		styleStr := styles.String()
 
 		if txt.Strike {
-			liStrike(f, txt.Content, txt.HREF)
+			liStrike(pdf, txt, styleStr)
 		} else if txt.Code {
-			liInlineCode(f, txt.Content, txt.HREF)
-		} else if txt.HREF != "" {
-			f.SetFont("helvetica", styleStr, 12)
-			f.SetTextColor(3, 102, 214)
-			f.WriteLinkString(lineHt, txt.Content, txt.HREF)
+			liInlineCode(pdf, txt)
 		} else {
-			liSpan(f, txt.Content, styleStr, lineHt)
+			liSpan(pdf, txt, styleStr, lineHt)
 		}
 	}
 }
 
-func liSpan(f *gofpdf.Fpdf, text string, styleStr string, lineHt float64) {
-	f.SetFont("helvetica", styleStr, 12)
-	f.SetFillColor(255, 255, 255)
-	f.SetTextColor(36, 41, 46)
-	f.Write(lineHt, text)
+func liSpan(pdf *gofpdf.Fpdf, txt Text, styleStr string, lineHt float64) {
+	pdf.SetFont("helvetica", styleStr, 12)
+	pdf.SetFillColor(255, 255, 255)
+	pdf.SetTextColor(36, 41, 46)
+	if txt.HREF != "" {
+		pdf.SetTextColor(3, 102, 214)
+		pdf.WriteLinkString(lineHt, txt.Content, txt.HREF)
+	} else {
+		pdf.SetTextColor(36, 41, 46)
+		pdf.Write(lineHt, txt.Content)
+	}
 }
 
-func liInlineCode(f *gofpdf.Fpdf, text, href string) {
-	oldCellMargin := f.GetCellMargin()
-	f.SetFillColor(243, 243, 243)
-	f.SetFont("courier", "", 12)
-	width := (f.GetStringWidth(text) * 1.05)
+func liInlineCode(pdf *gofpdf.Fpdf, txt Text) {
+	oldCellMargin := pdf.GetCellMargin()
+	pdf.SetFillColor(243, 243, 243)
+	pdf.SetFont("courier", "", 12)
+	width := (pdf.GetStringWidth(txt.Content) * 1.05)
+	pdf.SetCellMargin(2)
 
-	if len(href) != 0 {
-		f.SetTextColor(3, 102, 214)
-		f.CellFormat(
-			width, // width; If 0, the cell extends up to the right margin.
-			6,     // height;
-			text,  // txtStr;
-			"",    // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
-			0,     // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
-			"LM",  // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
-			true,  // fill; true for color, false for transparent
-			0,     // link;  Identifier returned by AddLink() or 0 for no internal link.
-			href,  // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
+	x := pdf.GetX()
+	pageWidth, _ := pdf.GetPageSize()
+	_, _, rightMargin, _ := pdf.GetMargins()
+	strWdth := pdf.GetStringWidth(txt.Content)
+	if x+strWdth > pageWidth-rightMargin {
+		pdf.Ln(-1)
+	}
+
+	if txt.HREF != "" {
+		pdf.SetTextColor(3, 102, 214)
+		pdf.CellFormat(
+			width,       // width; If 0, the cell extends up to the right margin.
+			6,           // height;
+			txt.Content, // txtStr;
+			"",          // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
+			0,           // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
+			"LM",        // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
+			true,        // fill; true for color, false for transparent
+			0,           // link;  Identifier returned by AddLink() or 0 for no internal link.
+			txt.HREF,    // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
 		)
 	} else {
-		f.CellFormat(
-			width, // width; If 0, the cell extends up to the right margin.
-			6,     // height;
-			text,  // txtStr;
-			"",    // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
-			0,     // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
-			"LM",  // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
-			true,  // fill; true for color, false for transparent
-			0,     // link;  Identifier returned by AddLink() or 0 for no internal link.
-			"",    // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
+		pdf.CellFormat(
+			width,       // width; If 0, the cell extends up to the right margin.
+			6,           // height;
+			txt.Content, // txtStr;
+			"",          // borderStr; "" (no border), "1" (full border), "L", "T", "R" and "B" (left, top, right and bottom)
+			0,           // ln; 0 (to the right), 1 (to the beginning of the next line, like Ln()), and 2 (below).
+			"LM",        // alignStr; Default is left middle. "L", "C" or "R" (left, center, right) + "T", "M", "B" or "A" (top, middle, bottom, baseline)
+			true,        // fill; true for color, false for transparent
+			0,           // link;  Identifier returned by AddLink() or 0 for no internal link.
+			"",          // linkStr; A target URL or empty for no external link. A non-zero value for link takes precedence over linkStr.
 		)
 	}
 
-	f.SetCellMargin(oldCellMargin)
+	pdf.SetCellMargin(oldCellMargin)
 }
 
-func liStrike(f *gofpdf.Fpdf, text string, href string) {
-	f.SetFont("helvetica", "", 12)
-	f.SetFillColor(255, 255, 255)
-	f.SetTextColor(36, 41, 46)
-	width := f.GetStringWidth(text)
+func liStrike(pdf *gofpdf.Fpdf, txt Text, styleStr string) {
+	pdf.SetFont("helvetica", styleStr, 12)
+	pdf.SetFillColor(255, 255, 255)
+	width := pdf.GetStringWidth(txt.Content)
 	var lineHt float64 = 6
 
 	// Where the text starts, also where to start the strikethrough line.
-	x := f.GetX()
-	y := (f.GetY() + (lineHt / 2))
+	x := pdf.GetX()
+	y := (pdf.GetY() + (lineHt / 2))
 
-	f.SetLineWidth(0.25)
-	f.SetDrawColor(36, 41, 46)
+	pdf.SetLineWidth(0.25)
+	pdf.SetDrawColor(36, 41, 46)
 
-	if len(href) != 0 {
-		f.SetTextColor(3, 102, 214)
-		f.WriteLinkString(lineHt, text, href)
+	if txt.HREF != "" {
+		pdf.SetTextColor(3, 102, 214)
+		pdf.WriteLinkString(lineHt, txt.Content, txt.HREF)
 	} else {
-		f.Write(lineHt, text)
+		pdf.SetTextColor(36, 41, 46)
+		pdf.Write(lineHt, txt.HREF)
 	}
 
-	f.Line(x, y, x+width, y)
+	pdf.Line(x, y, x+width, y)
 }
