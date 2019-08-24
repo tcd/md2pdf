f3ab7d12ab7ed2cdb27c2ea8f9ce58c98c586072
Clay Dunston <claydunston@gmail.com>
2019-08-23 23:43:05 -0500

    Update codeblock rendering

--- internal/render/codeblock.go
+++ internal/render/codeblock.go
@@ -1,20 +1,29 @@
 package render
 
-import "github.com/jung-kurt/gofpdf"
+import (
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+)
 
 // CodeBlock writes a `<pre>` style code block to a gofpdf.Fpdf.
-// TODO: syntax highlighting.
-func CodeBlock(f *gofpdf.Fpdf, text string) {
-	oldCellMargin := f.GetCellMargin()
+func CodeBlock(pdf *gofpdf.Fpdf, contents Contents) {
+	if len(contents.Content) != 1 {
+		fmt.Println("CodeBlock: invalid argument.")
+	}
 
-	f.SetFont("courier", "", 11)
-	f.SetFillColor(246, 248, 250)
-	f.SetCellMargin(4)
+	content := contents.Content[0].Content
 
-	f.CellFormat(0, 4, "", "", 1, "TL", true, 0, "")
-	f.MultiCell(0, 5, text, "", "", true)
-	f.CellFormat(0, 4, "", "", 1, "TL", true, 0, "")
-	f.Ln(6)
+	oldCellMargin := pdf.GetCellMargin()
 
-	f.SetCellMargin(oldCellMargin)
+	pdf.SetFont("courier", "", 11)
+	pdf.SetFillColor(246, 248, 250)
+	pdf.SetCellMargin(4)
+
+	pdf.CellFormat(0, 4, "", "", 1, "TL", true, 0, "")
+	pdf.MultiCell(0, 5, content, "", "", true)
+	pdf.CellFormat(0, 4, "", "", 0, "TL", true, 0, "")
+	pdf.Ln(6)
+
+	pdf.SetCellMargin(oldCellMargin)
 }
