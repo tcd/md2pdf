0360e22a289c42576e61cb95102af00c8bae4f94
Clay Dunston <claydunston@gmail.com>
2019-08-28 12:39:31 -0500

    Move parseContent to its own file

---
+++ internal/parse/content.go
@@ -0,0 +1,65 @@
+package parse
+
+import (
+	"github.com/tcd/md2pdf/internal/render"
+	"golang.org/x/net/html"
+)
+
+func parseContent(
+	z *html.Tokenizer,
+	startToken html.Token,
+	parent render.Text,
+	topLevel *render.Contents,
+) {
+	this := parent.Copy()
+	currentKind := startToken.Data
+
+	if currentKind == "p" {
+		return
+	}
+
+	if currentKind == "a" {
+		for _, a := range startToken.Attr {
+			if a.Key == "href" {
+				this.HREF = a.Val
+			}
+		}
+	}
+	if currentKind == "strong" {
+		this.Bold = true
+	}
+	if currentKind == "em" {
+		this.Italic = true
+	}
+	if currentKind == "code" {
+		this.Code = true
+	}
+	if currentKind == "del" {
+		this.Strike = true
+	}
+
+	for {
+		tt := z.Next()
+		if tt == html.ErrorToken {
+			break
+		}
+		if tt == html.EndTagToken {
+			T2 := z.Token()
+			if T2.Data == currentKind {
+				break
+			}
+		}
+		if tt == html.StartTagToken {
+			T2 := z.Token()
+			parseContent(z, T2, this, topLevel)
+		}
+		if tt == html.TextToken {
+			this.Content = string(z.Text())
+		}
+		if tt == html.SelfClosingTagToken {
+			continue
+		}
+	}
+
+	topLevel.AddContent(this)
+}
