efebafb33537db990dc90c71381404f7185a17a3
Clay Dunston <claydunston@gmail.com>
2019-08-28 18:47:04 -0500

    Rename renderable/pdf to renderable/elements

--- internal/renderable/elements.go
+++ internal/renderable/elements.go
@@ -5,21 +5,20 @@ import (
 	"github.com/tcd/md2pdf/internal/render"
 )
 
-// PDF attaches useful methods to an Fpdf.
-type PDF struct {
-	*gofpdf.Fpdf // https://github.com/jung-kurt/gofpdf/blob/master/def.go#L499
-	Elements     []Renderable
-}
-
 // Elements slice of Renderables that can be rendered to a PDF.
 type Elements struct {
-	Elements []Renderable
+	Elements []Renderable `json:"elements"`
 }
 
 // Add an Element.
 func (e *Elements) Add(element Renderable) {
 	e.Elements = append(e.Elements, element)
 }
+
+// // Add one or more elements.
+// func (e *Elements) Add(elements ...Renderable) {
+// 	e.Elements = append(e.Elements, elements...)
+// }
 
 // Render a PDF from Elements.
 func (e Elements) Render(outPath string) (err error) {
@@ -31,8 +30,3 @@ func (e Elements) Render(outPath string) (err error) {
 	err = pdf.OutputFileAndClose(outPath)
 	return
 }
-
-// // Add one or more elements.
-// func (e *Elements) Add(elements ...Renderable) {
-// 	e.Elements = append(e.Elements, elements...)
-// }
