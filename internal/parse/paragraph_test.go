bad086547750ddaaffe613b31a29eff54c43df47
Clay Dunston <claydunston@gmail.com>
2019-08-30 19:26:48 -0500

    Add failing paragraph parse test

---
+++ internal/parse/paragraph_test.go
@@ -0,0 +1,38 @@
+package parse
+
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/tcd/md2pdf/internal/model"
+	"golang.org/x/net/html"
+)
+
+// This is actually testing parseContent.
+func TestParseP(t *testing.T) {
+	input := `<p>Combined emphasis with <strong>asterisks and <em>underscores</em></strong>.</p>`
+	want := model.Contents{}
+	want.AddStr("Combined emphasis with ")
+	want.AddContent(model.Text{
+		Text: "asterisks and ",
+		Bold: true,
+	})
+	want.AddContent(model.Text{
+		Text:   "underscores",
+		Bold:   true,
+		Italic: true,
+	})
+	want.AddStr(".")
+
+	have, _ := parseP(html.NewTokenizer(strings.NewReader(input)))
+
+	areEqual, err := AreStructsEqual(have, want)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !areEqual {
+		t.Errorf("parseP: results do not match")
+	}
+}
