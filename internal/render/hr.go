9ddd95578045a4cdd7567dd50c2b956c4fa20970
Clay Dunston <claydunston@gmail.com>
2019-09-03 12:31:54 -0500

    Update render/hr

--- internal/render/hr.go
+++ internal/render/hr.go
@@ -1,13 +1,22 @@
 package render
 
-import "github.com/jung-kurt/gofpdf"
+import (
+	"github.com/jung-kurt/gofpdf"
+	"github.com/tcd/md2pdf/internal/lib"
+)
 
-// HR writes a Horizontal Rule to a gofpdf.Fpdf.
+// HR draws a standard Horizontal Rule to a gofpdf.Fpdf.
 func HR(pdf *gofpdf.Fpdf) {
+	pdf.Ln(2)
+	hr(pdf, 1.00)
+	pdf.Ln(2)
+}
+
+// hr draws a horizontal rule with a given width.
+func hr(pdf *gofpdf.Fpdf, lineWidth float64) {
 	x := pdf.GetX()
 	y := pdf.GetY()
-	pdf.SetLineWidth(1)
+	pdf.SetLineWidth(lineWidth)
 	pdf.SetDrawColor(HrFG())
-	pdf.Line(x, y, x+pageWidth, y)
-	pdf.Ln(2)
+	pdf.Line(x, y, x+lib.ContentBoxWidth(pdf), y)
 }
